f231cb0f508bc3a84ff70ae21a72469f4efd48cd
Vinu-AD <vinu882368@gmail.com>
2025-12-29 11:57:47 +0530

    fix: added backend api url in .env

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: VITE_API_BASE_URL,
+  // baseURL: "http://localhost:8080",
 });
 
 api.interceptors.response.use(
