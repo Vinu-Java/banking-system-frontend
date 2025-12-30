f79c127bda0c61fefcbbf60aa3a4c8ca93efe311
Vinu-AD <vinu882368@gmail.com>
2025-12-30 10:59:11 +0530

    fix: change in baseURL link

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: "http://localhost:8080",
 });
 
 api.interceptors.response.use(
